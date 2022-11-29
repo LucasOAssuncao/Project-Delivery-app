4b48153197b8c00004dc26ec07ea1873f1607607
￼Marcio-Gabriel-Roque-Mendes <marcio_2411@hotmail.com>
2022-11-29 19:13:45 -0300

    Feat: importacao do router dom

--- front-end/src/components/NavGeneric.jsx
+++ front-end/src/components/NavGeneric.jsx
@@ -1,20 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavGeneric() {
-    return (
-        <header>
-        <nav>
+  return (
+    <header>
+      <nav>
         <Link to="/" data-testid="">Produtos</Link>
         {' '}
         <Link to="/" data-testid="">Meus Pedidos</Link>
-        </nav>
+      </nav>
 
-        <nav>
-          <Link to="/" data-testid="">Nome Cliente/Vendedor</Link>
-          {' '}
-          <Link to="/" data-testid="">Sair</Link>
-        </nav>
-      </header>
-    );
+      <nav>
+        <Link to="/" data-testid="">Nome Cliente/Vendedor</Link>
+        {' '}
+        <Link to="/" data-testid="">Sair</Link>
+      </nav>
+    </header>
+  );
 }
 export default NavGeneric;
--- front-end/src/components/NavGenericFunc.jsx
+++ front-end/src/components/NavGenericFunc.jsx
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function NavGeneric({produtos,meuPedidos, nomePessoa, sair}) {
-    return (
-        <header>
-        <nav>
+function NavGeneric({ produtos, meuPedidos, nomePessoa, sair }) {
+  return (
+    <header>
+      <nav>
         <Link to="/" data-testid="">{produtos}</Link>
         {' '}
         <Link to="/" data-testid="">{meuPedidos}</Link>
-        </nav>
+      </nav>
 
-        <nav>
-          <Link to="/" data-testid="">{nomePessoa}</Link>
-          {' '}
-          <Link to="/" data-testid="">{sair}</Link>
-        </nav>
-      </header>
-    );
-  }
+      <nav>
+        <Link to="/" data-testid="">{nomePessoa}</Link>
+        {' '}
+        <Link to="/" data-testid="">{sair}</Link>
+      </nav>
+    </header>
+  );
+}
 export default NavGeneric;
 
-NavGeneric.prototype = {
-    produtos: PropTypes.string,
-    meuPedidos: PropTypes.string,
-    nomePessoa: PropTypes.string, 
-    sair: PropTypes.string,   
-}
+NavGeneric.propTypes = {
+  produtos: PropTypes.string,
+  meuPedidos: PropTypes.string,
+  nomePessoa: PropTypes.string,
+  sair: PropTypes.string,
+}.isRequired;
