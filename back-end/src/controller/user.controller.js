48cd423b51a499304a03dc416642035b47228439
Fernanda Vidal <fevidal.dev@gmail.com>
2022-11-30 18:16:42 -0300

    fix: complexity.

--- back-end/src/controller/user.controller.js
+++ back-end/src/controller/user.controller.js
@@ -4,15 +4,15 @@ const usersService = require('../services/usersService');
 const userController = {
   signUp: async (req, res) => {
     const { name, email, password } = req.body;
-    const cryptPassword = bcrypt.hashSync(
+    const cryptoPassword = bcrypt.hashSync(
       password,
       bcrypt.genSaltSync(12),
       null,
     );
 
-    await usersService.create({ name, email, cryptPassword });
+    await usersService.create({ name, email, cryptoPassword });
 
-    res.status(200).json({ message: 'User created!' });
+    res.status(201).json({ message: 'User created!' });
   },
 };
 
--- back-end/src/middlewares/verifyCredentials.js
+++ back-end/src/middlewares/verifyCredentials.js
@@ -1,15 +1,17 @@
-const usersService = require('../services/usersService');
-
-const verifyCredentials = async (req, res, next) => {
-  const { email, password } = req.body;
+const verifyCredentials = (req, res, next) => {
+  const { name, email, password } = req.body;
 
   if (!email) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
 
+  if (name.length < 12) { 
+    return res.status(400).json({ message: 'Name must have at least 12 characters' });
+  }
+
   if (password.length < 6) {
     return res.status(400)
-      .json({ message: 'Password must be at least 6 characters' });
+      .json({ message: 'Password must have at least 6 characters' });
   }
 
   const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
@@ -18,10 +20,6 @@ const verifyCredentials = async (req, res, next) => {
       message: 'Email must have a valid format',
     });
   }
-
-  const verifyUser = await usersService.findByEmail(email);
-
-  if (verifyUser) res.status(401).json({ message: 'User already exists' });
 
   next();
 };
