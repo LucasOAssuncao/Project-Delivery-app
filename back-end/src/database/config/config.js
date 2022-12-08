7e32e7f011538d10b8836ab7d091f6067bb0fc66
Lucas Otoni <lotoni.vivo@gmail.com>
2022-12-08 19:04:22 -0300

    fix: fix config db

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -14,7 +14,7 @@ const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix.dev}`,
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
