3c44fdf27ac9bde394e069143c176583e1aae5f8
Fernanda Vidal <fevidal.dev@gmail.com>
2022-12-09 22:06:12 -0300

    fix: Retira importação authMiddleware

--- back-end/src/routes/register.js
+++ back-end/src/routes/register.js
@@ -2,11 +2,9 @@ const express = require('express');
 const userController = require('../controller/user.controller');
 const verifyCredentials = require('../middlewares/verifyCredentials');
 const verifyUser = require('../middlewares/verifyUser');
-const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.get('/', userController.getAllUsers);
 router.post('/', verifyCredentials, verifyUser, userController.signUp);
 
 module.exports = router;
