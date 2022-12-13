95f84d4ca3af349ddbb6e34429c3f25bce175e7c
Fernanda Vidal <fevidal.dev@gmail.com>
2022-12-13 18:34:08 -0300

    fix: Redirecionamento tela login.

--- front-end/src/pages/PedidoStatus/PedidoStatus.jsx
+++ front-end/src/pages/PedidoStatus/PedidoStatus.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import CardPedidoStatus from '../../components/cardPedidoStatus';
+import NavBar from '../../components/Nav';
 
 function PedidoStatus() {
   return (
     <div>
+      <NavBar />
       <CardPedidoStatus />
     </div>
   );
